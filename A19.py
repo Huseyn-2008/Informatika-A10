a8b9ecc70ec319257d7dd2c7829ae84b63730e05
Only Up <76998342+Huseyn-2008@users.noreply.github.com>
2022-11-16 21:01:57 +0400

    Create A19.py

---
+++ A19.py
@@ -0,0 +1,17 @@
+#Works if len(num) == 5
+
+num = input("Num: ")
+
+x = int(num[0])
+y = int(num[1])
+z = x + y
+
+x1 = int(num[3])
+y1 = int(num[4])
+z1 = x1 + y1
+
+print(z - z1)
+
+#OUTPUT:
+#Num: 12345
+#  -6
